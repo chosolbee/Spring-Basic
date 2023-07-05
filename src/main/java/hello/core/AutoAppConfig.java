3f9dde1daaa1c4fbb09ca8bdcabd050b6293f565
chosolbee <sbcho0325@gmail.com>
2023-07-05 22:57:44 +0900

    section 6

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,4 +1,25 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan( //@Component가 붙은 클래스를 모두 찾아서 자동으로 스프링 빈으로 등록
+        // basePackages = "hello.core.member", // 해당 package 내 @Component만 등록하도록
+        // basePackageClasses = AutoAppConfig.class, // 내가 속한 패키지 부터 (여기서는 hello.core)
+        excludeFilters = @ComponentScan.Filter( // 그중에서 뺄 것들 지정
+              // AppConfig는 현재 수동으로 등록하기 때문에 자동으로 등록되면 안된다 (충돌 발생) - 우리의 예제를 잘 유지하기 위함
+              type = FilterType.ANNOTATION, classes = Configuration.class
+      )
+)
+
 public class AutoAppConfig {
+      @Bean(name = "memoryMemberRepository")
+      MemberRepository memberRepository(){
+            return new MemoryMemberRepository();
+      }
 }
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,12 +1,18 @@
 package hello.core.member;
 
 //구현체 하나만 있을 때는 관례상 interface명 뒤에 Impl이라 많이 사용
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     //가입하고 회원을 찾으려면 MemberRepository가 필요
     private final MemberRepository memberRepository; //인터페이스에만 의존하도록
 
     //어떤 memberRepository가 들어올지 아직 모름 -> AppConfig에서 정하겠다
+    @Autowired //ac.getBean(MemberRepository.class)
     public MemberServiceImpl(MemberRepository memberRepository) {
 
         this.memberRepository = memberRepository;
